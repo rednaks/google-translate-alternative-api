fcaea8e534f8a8df4342682dd596df9c1f80683d
Alexandre BM <rednaks@pro-programming.net>
2012-06-04 14:20:57 +0200

    reorganizing code for the Hackathon

--- index.py
+++ index.py
@@ -1,50 +1,10 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
 
-import pycurl
-import urllib
-import StringIO
-from lib.HTMLParser import HTMLParser
+import mods.AlternativeApi
 import cgi
 
 
-
-found = False
-my_text = ''
-    
-class MyHTMLParser(HTMLParser):
-
-    def handle_starttag(self, tag, attrs):
-        global found
-        try:
-            if(attrs[0] == ('id','result_box')): 
-                found = True             
-        except:
-            i=1 # Nothing to do, so i = 1 :D
-            
-    def handle_endtag(self, tag):
-        global found
-        if(tag == 'div'):
-            found = False
-            
-    def handle_data(self, data):
-        global found
-        global my_text
-        if(found):
-         my_text = my_text+ data
-
-
-def curl(text):
-	URL = 'http://translate.google.com/'
-	data = {'sl':'auto','tl':'en','js':'n','prev':'_t','hl':'en','ie':'UTF-8','layout':'2','eotf':'1','text':text,'file':''}
-	handle = pycurl.Curl()
-	b = StringIO.StringIO()
-	handle.setopt(pycurl.URL,URL)
-	handle.setopt(pycurl.POST,1)
-	handle.setopt(pycurl.POSTFIELDS,urllib.urlencode(data))
-	handle.setopt(pycurl.WRITEFUNCTION,b.write)
-	handle.perform()
-	return b.getvalue()
 
 
 form = cgi.FieldStorage()        
