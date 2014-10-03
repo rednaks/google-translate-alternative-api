3a063f1fc592e7bfe210e792f55218c70bc08d95
Alexandre BM <s@rednaks.tn>
2014-10-03 01:11:53 +0100

    Flask Restful API

--- index.py
+++ index.py
@@ -2,32 +2,33 @@
 #-*- coding: utf-8 -*-
 
 from  mods.AlternativeApi import *
-import cgi
+
+from flask import Flask, render_template, request, jsonify, abort
 
 
 
-
-form = cgi.FieldStorage()        
-print 'Content-type: text/html'
-print 
-
-
-get_data = form.getvalue('text')
-json = form.getvalue('json')
-if(get_data is not None):
-	my_page = curl(cgi.escape(form.getvalue('text')))
-	my_text = BeautifulParser(my_page);
-	if(json == 'true'):
-		my_text = '{"source": "%s", "translated": "%s"}' %(get_data,my_text)
-
-	print my_text,
-else:
-	print '''
-			<h1> 404 not found !</h1>
-			<p> seriously ? :D try with rednaks.alwaysdata.net/translate/?text=&lt;text to translate&gt;</p>
-			<p> To get json format add json=true option</p>'''
+app = Flask(__name__)
 
 
 
+@app.route('/api/v1/', methods=['GET'])
+def translate():
+  text = ''
+  text = request.args['text']
+
+  page = curl(text)
+  tr_text = BeautifulParser(page)
+  if(len(tr_text) == 0):
+    return abort(503)
+
+  return jsonify({'original':text,'translated':tr_text})
 
 
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+
+if __name__ == '__main__':
+  app.run(debug = True, host = '0.0.0.0')
